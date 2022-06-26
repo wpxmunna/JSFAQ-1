d7b3e265688550563ac4f3f2b997988800365d44
wpxmunna <wpxmunna@gmail.com>
2022-06-27 01:58:26 +0600

    setTimeout and setinterval

---
+++ set-time-out.js
@@ -0,0 +1,14 @@
+function doSomething(){
+    console.log(4444);
+}
+console.log(1234);
+setTimeout(doSomething, 10000);
+console.log(5678);
+setTimeout(function(){
+    console.log('lazy and waiting');
+},15000);
+console.log(9874);
+
+setInterval(() => {
+    console.log('Doing it..');
+}, 1000);
